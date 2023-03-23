5bd18d8cbd067b6598a9d3c14a859067b78460fb
Reethika Kasarla <reethika@auto.ece.pdx.edu>
2023-03-22 20:26:24 -0700

    team_5

---
+++ constraints/genus.cmd
@@ -0,0 +1,15 @@
+# Cadence Genus(TM) Synthesis Solution, Version 19.12-s121_1, built Dec  3 2019 15:07:17
+
+# Date: Wed Mar 22 15:30:12 2023
+# Host: mo.ece.pdx.edu (x86_64 w/Linux 3.10.0-1160.81.1.el7.x86_64) (1core*32cpus*32physical cpus*Intel Xeon E312xx (Sandy Bridge, IBRS update) 16384KB)
+# OS:   CentOS Linux release 7.9.2009 (Core)
+
+pwd
+read_power_intent -1801 filename.upf
+read_power_intent -1801 ORCA_TOP.upf
+o
+set top_design ORCA_TOP
+read_power_intent -1801 ORCA_TOP.upf
+read_power_intent
+read_power_intent -1801 ORCA_TOP.upf
+exit
